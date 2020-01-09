92902b7c5f34aa8e41ec58f5b32ac14a4a23a204
thrucker <t.rucker@xpure.de>
2020-01-09 12:14:43 +0100

    Use vessel service

--- go.mod
+++ go.mod
@@ -3,21 +3,8 @@ module github.com/thrucker/consignment-service
 go 1.13
 
 require (
-	github.com/coreos/etcd v3.3.18+incompatible // indirect
-	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/protobuf v1.3.2
-	github.com/lucas-clemente/quic-go v0.14.1 // indirect
 	github.com/micro/go-micro v1.18.0
-	github.com/miekg/dns v1.1.25 // indirect
-	github.com/nats-io/jwt v0.3.2 // indirect
-	go.uber.org/atomic v1.5.1 // indirect
-	go.uber.org/multierr v1.4.0 // indirect
-	go.uber.org/zap v1.13.0 // indirect
-	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413 // indirect
-	golang.org/x/lint v0.0.0-20191125180803-fdd1cda4f05f // indirect
+	github.com/thrucker/vessel-service v0.0.0-20200109103324-1ba1b39c22f7
 	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553
-	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
-	golang.org/x/tools v0.0.0-20191213032237-7093a17b0467 // indirect
-	google.golang.org/genproto v0.0.0-20191206224255-0243a4be9c8f // indirect
-	google.golang.org/grpc v1.25.1
 )
--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"github.com/micro/go-micro"
 	pb "github.com/thrucker/consignment-service/proto/consignment"
+	"github.com/thrucker/vessel-service/proto/vessel"
+	"log"
+	"sync"
 )
 
 const (
-	port =":50051"
+	port = ":50051"
 )
 
 type repository interface {
@@ -17,23 +20,39 @@ type repository interface {
 }
 
 type Repository struct {
+	mu           sync.RWMutex
 	consignments []*pb.Consignment
 }
 
 func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
+	repo.mu.Unlock()
 	return consignment, nil
-}
-type service struct {
-	repo repository
 }
 
 func (repo *Repository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
 
+type service struct {
+	repo         repository
+	vesselClient vessel.VesselServiceClient
+}
+
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vessel.Specification{
+		MaxWeight: req.Weight,
+		Capacity:  int32(len(req.Containers)),
+	})
+	if err != nil {
+		return err
+	}
+	log.Printf("Found vessel: %s\n", vesselResponse.Vessel.Name)
+
+	req.VesselId = vesselResponse.Vessel.Id
+
 	consignment, err := s.repo.Create(req)
 	if err != nil {
 		return err
@@ -41,11 +60,10 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, re
 
 	res.Created = true
 	res.Consignment = consignment
-
 	return nil
 }
 
-func (s *service) GetConsignments(ctx context.Context,  req *pb.GetRequest, res *pb.Response) error {
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
 	consignments := s.repo.GetAll()
 	res.Consignments = consignments
 	return nil
@@ -55,12 +73,14 @@ func main() {
 	repo := &Repository{}
 
 	srv := micro.NewService(
-		micro.Name("shippy.consignment.service"),
+		micro.Name("shippy.service.consignment"),
 	)
 
 	srv.Init()
 
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	vesselClient := vessel.NewVesselServiceClient("shippy.service.vessel", srv.Client())
+
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 
 	if err := srv.Run(); err != nil {
 		fmt.Println(err)
